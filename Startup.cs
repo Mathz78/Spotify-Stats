46da09794ae82c6604fe3dacca731e12218ffa40
Matheus <matheusrocha7802@gmail.com>
2022-04-23 14:09:42 -0300

    Adding swagger

--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using RestEase;
 using SpotifyStats.Facades;
 using SpotifyStats.Interfaces;
@@ -33,6 +27,14 @@ namespace SpotifyStats
         {
             services.AddControllers();
 
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new OpenApiInfo 
+                {
+                    Title = "Spotify Stats", 
+                    Version = "v1"
+                });
+            });
+        
             var apiSettings = Configuration.GetSection("ApiSettings").Get<ApiSettings>();
 
             services.AddSingleton(apiSettings)
@@ -50,6 +52,12 @@ namespace SpotifyStats
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Spotify Stats v1");
+            });
 
             app.UseHttpsRedirection();
 
